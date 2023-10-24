8090743545638bc26661f3b8b5d43daa5461ffe3
N RAMESH KANNAN <43806145+kannan-ramesh@users.noreply.github.com>
2023-10-24 20:52:05 +0530

    update user input

--- arrayZoho/src/arrayZoho/matrixMultiply.java
+++ arrayZoho/src/arrayZoho/matrixMultiply.java
@@ -5,14 +5,47 @@ import java.util.Scanner;
 public class matrixMultiply {
 
 	public static void main(String[] args) {
-		Scanner input=new Scanner(System.in);
-		
-		int arr[][] = {	{1,2},{3,4}};
-		int arr1[][] = {{5,6,7},{8,9,10}};
-		
-		int ans[][]= new int[2][3];
+		 Scanner input = new Scanner(System.in);
+//			{	{1,2},{3,4}};
+//			{{5,6,7},{8,9,10}};
+			
+	        System.out.println("Enter first array(matrix) (row and column):");
+	        int row1 = input.nextInt();
+	        int col1 = input.nextInt();
+
+	        System.out.println("Enter  second array (row and column):");
+	        int row2 = input.nextInt();
+	        int col2 = input.nextInt();
+	        
+	        if (col1 != row2) {
+	            System.out.println("Matrix multiplication is not possible.");
+	            return;
+	        }
+
+	        int arr[][] = new int[row1][col1];
+	        int arr1[][] = new int[row2][col2];
+
+	        System.out.println("Enter elements first array:");
+	        for (int i = 0; i < row1; i++) {
+	            for (int j = 0; j < col1; j++) {
+	                arr[i][j] = input.nextInt();
+	            }
+	        }
+
+	        System.out.println("Enter elements of the second array:");
+	        for (int i = 0; i < row2; i++) {
+	            for (int j = 0; j < col2; j++) {
+	                arr1[i][j] = input.nextInt();
+	            }
+	        }
+	        
+		int ans[][]= new int[row1][col2];
 		int sum=0;
-		
+		//call method multiplication in two array
+		matrixmultiplication(arr,arr1,ans,sum);
+	}
+
+	private static void matrixmultiplication(int[][] arr, int[][] arr1, int[][] ans, int sum) {
 		for(int i=0;i<2;i++) {
 			for(int j=0;j<3;j++) {
 				for(int k=0;k<ans.length;k++) {
@@ -22,11 +55,10 @@ public class matrixMultiply {
 				sum=0;
 			}
 		}
-		
+		//print array
 		for(int i=0;i<ans.length;i++) {
 			System.out.println(Arrays.toString(ans[i]));
 		}
-
 	}
 
 }
